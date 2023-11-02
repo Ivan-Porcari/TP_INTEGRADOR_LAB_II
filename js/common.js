7a7a1b2f19f09a36ce14612e70e31a099299ac36
Ivan-Porcari <ivanporcari93@gmail.com>
2023-11-01 21:25:04 -0300

    arreglo html/css

--- js/common.js
+++ js/common.js
@@ -8,6 +8,7 @@
 // const tipoRecuento = 1;
 
 // API MINISTERIO DEL INTERIOR
+// falta sumar boton de generales/paso para filtrar
 const url = "https://resultados.mininterior.gob.ar/api/menu"
 const urlPeriodos = "https://resultados.mininterior.gob.ar/api/menu/periodos"
 const urlCargos =  "https://resultados.mininterior.gob.ar/api/menu?año="
@@ -23,36 +24,37 @@ fetch (urlPeriodos)
         const option = document.createElement('option');
         option.text = periodo;
         option.value = periodo;
-        periodosSelect.add(option);
-    
+        periodosSelect.add(option);   
         
 });
     console.log(data)
 })
 .catch(error => consolog.log(error))
 
-
-if (periodosSelect.value) {
-    fetch(urlCargos + periodosSelect.value) //periodosSelect = eleccion del año por el usuario
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); // por alguna cierta razón no estoy trayendo los datos del periodo
-            const cargosSelect = document.getElementById('select-cargo'); // ID del botón de HTML
-            
-            while (cargosSelect.firstChild) {
-                cargosSelect.removeChild(cargosSelect.firstChild);
-            }
-            
-            data.forEach(eleccion => {
-                eleccion.Cargos.forEach(cargo => {
-                    const option = document.createElement('option');
-                    option.text = cargo.Cargo;
-                    option.value = cargo.IdCargo;
-                    cargosSelect.add(option);
+//llamar a una función para toma rel valor del select-aa
+function seleccionarAnio() {
+    if (periodosSelect.value) {
+        fetch(urlCargos + periodosSelect.value) //periodosSelect = eleccion del año por el usuario
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // por alguna cierta razón no estoy trayendo los datos del periodo
+                const cargosSelect = document.getElementById('select-cargo'); // ID del botón de HTML
+                
+                while (cargosSelect.firstChild) {
+                    cargosSelect.removeChild(cargosSelect.firstChild);
+                }
+                
+                data.forEach(eleccion => {
+                    eleccion.Cargos.forEach(cargo => {
+                        const option = document.createElement('option');
+                        option.text = cargo.Cargo;
+                        option.value = cargo.IdCargo;
+                        cargosSelect.add(option);
+                    });
                 });
-            });
-        })
-        .catch(error => console.log(error));
-} else {
-    console.log("No se ha seleccionado un período.");
+            })
+            .catch(error => console.log(error));
+    } else {
+        console.log("No se ha seleccionado un período.");
+    }
 }
